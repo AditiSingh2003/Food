75ecf2ce6244e1d3a5c7eb11706e714ab29801ad
Aditi <adithakurti@gmail.com>
2023-07-16 17:36:46 +0530

    added welcome to main screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:food/Screen/Welcome.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:sizer/sizer.dart';
+import 'package:food/Screen/Splash.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,23 +10,44 @@ void main() async {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: App(),
+      home: Welcome(),
     )
   );
 }
 
-class App extends StatefulWidget {
-  const App({super.key});
+
+class Welcome extends StatefulWidget {
+  const Welcome({Key? key}) : super(key: key);
 
   @override
-  State<App> createState() => _AppState();
+  State<Welcome> createState() => _WelcomeState();
 }
 
-class _AppState extends State<App> {
+class _WelcomeState extends State<Welcome> 
+with SingleTickerProviderStateMixin{
+
+
   @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3),(){
+       Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Splash(),
+              ),
+            );
+    });
+  }
+
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Welcome(),
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(image: AssetImage('assets/Splash_screen_4.png'),
+        fit: BoxFit.cover
+      ),
+      ),
+      
     );
   }
 }
