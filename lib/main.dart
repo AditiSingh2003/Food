a3d7fc17dd3936d7e66b5b064735d66f2f3a4d77
Aditi <adithakurti@gmail.com>
2023-07-16 18:10:06 +0530

    testing by writing welcome

--- lib/main.dart
+++ lib/main.dart
@@ -18,18 +18,18 @@ void main() async {
 
 class Welcome extends StatefulWidget {
   const Welcome({Key? key}) : super(key: key);
-
   @override
   State<Welcome> createState() => _WelcomeState();
 }
 
 class _WelcomeState extends State<Welcome> {
-
-
   @override
 
   Widget build(BuildContext context) {
     return Container(
+      height: 200,
+      width: 200,
+      child: Text('Welcome',style: TextStyle(fontSize: 30,color:Colors.black),textAlign: TextAlign.center,),
       color: Colors.red,
     );
   }
